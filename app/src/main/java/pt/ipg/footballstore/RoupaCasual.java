fe7b2293c925b12267231741e37dd3e9adc788bd
danidpina <pinadani@hotmail.com>
2019-04-02 18:58:19 +0100

    Atividade roupa casual completa

--- app/src/main/java/pt/ipg/footballstore/RoupaCasual.java
+++ app/src/main/java/pt/ipg/footballstore/RoupaCasual.java
@@ -18,6 +18,9 @@ public class RoupaCasual extends AppCompatActivity {
     private Button botaoguardar;
     private  Button botaocancelar;
     private EditText edittextnome;
+    private EditText edittextquantidade;
+    private EditText edittextpreco;
+    private EditText edittexttamanho;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +30,35 @@ public class RoupaCasual extends AppCompatActivity {
 
         botaoguardar = (Button) findViewById(R.id.buttonGuardar);
         botaocancelar= (Button) findViewById(R.id.buttonCancelar);
-        edittextnome= (EditText) findViewById(R.id.editText);
+        edittextnome= (EditText) findViewById(R.id.EditTextNome);
+        edittextquantidade= (EditText) findViewById(R.id.EditTextQuantidade);
+        edittextpreco = (EditText) findViewById(R.id.EditTextPreco);
+        edittexttamanho= (EditText) findViewById(R.id.EditTextTamanho);
         botaoguardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String mensagem = edittextnome.getText().toString();
+                if (mensagem.trim().length()==0){
+                    edittextnome.setError("Escreva algo");
+                }else{
+                    finish();
+                    Toast.makeText(RoupaCasual.this, "Peça guardada", Toast.LENGTH_SHORT).show();
+                }
+                String mensagem1 = edittextquantidade.getText().toString();
+                if (mensagem.trim().length()==0){
+                    edittextnome.setError("Escreva algo");
+                }else{
+                    finish();
+                    Toast.makeText(RoupaCasual.this, "Peça guardada", Toast.LENGTH_SHORT).show();
+                }
+                String mensagem2 = edittexttamanho.getText().toString();
+                if (mensagem.trim().length()==0){
+                    edittextnome.setError("Escreva algo");
+                }else{
+                    finish();
+                    Toast.makeText(RoupaCasual.this, "Peça guardada", Toast.LENGTH_SHORT).show();
+                }
+                String mensagem3 = edittextpreco.getText().toString();
                 if (mensagem.trim().length()==0){
                     edittextnome.setError("Escreva algo");
                 }else{
